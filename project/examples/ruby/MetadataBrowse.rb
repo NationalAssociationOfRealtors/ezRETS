ad3d32343366222736dfb6ddf5b2da91848423de
Keith T. Garner <kgarner@realtors.org>
2006-04-01 17:36:07 +0000

    First pass at Metadata dumping script
    git-svn-id: https://code.crt.realtors.org/svn/odbcretsdrv/ezrets/trunk@911 fe7ac059-47f5-0310-8599-d0339b4e125c

---
+++ project/examples/ruby/MetadataBrowse.rb
@@ -0,0 +1,71 @@
+#!/usr/bin/env ruby
+# This ruby example requires the ruby odbc library which can be found
+# at http://www.ch-werner.de/rubyodbc/  Unfortunately, this isn't available
+# as a gem as of yet.
+
+$VERBOSE = 1
+
+require 'odbc'
+require 'optparse'
+
+DRIVER = "ezRETS ODBC Driver"
+drv = ODBC::Driver.new
+drv.name = DRIVER
+drv.attrs["DRIVER"] = DRIVER
+drv.attrs["LoginUrl"] = "http://demo.crt.realtors.org:6103/rets/login"
+drv.attrs["UID"] = "Joe"
+drv.attrs["PWD"] = "Schmoe"
+drv.attrs["StandardNames"] = "false"
+
+usage = OptionParser.new do |opts|
+  opts.banner = "Usage: GenericSearch.rb [options] <sql query>"
+  opts.on('-b', '--bulk_metadata', 'Grab the metadata all at once') do |a|
+    drv.attrs["UseBulkMetadata"] = "true"
+  end
+  opts.on('-dARG', '--debug_log ARG', 'Debug log to given file') do |a|
+    drv.attrs["DebugLogFile"] = a
+  end
+  opts.on('-HARG', '--http_log ARG', 'HTTP log to given file') do |a|
+    drv.attrs["HttpLogFile"] = a
+  end
+  opts.on('-lARG', '--loginUrl ARG', 'Login URL') do |a|
+    drv.attrs["LoginUrl"] = a
+  end
+  opts.on('-pARG', '--password ARG') { |a| drv.attrs["PWD"] = a }
+  opts.on('-rARG', '--rets_version ARG') { |a| drv.attrs["RetsVersion"] = a }
+  opts.on('-s', '--standard_names', 'Use standard names') do
+    drv.attrs["StandardNames"] = "true"
+  end
+  opts.on('-uARG', '--user ARG') { |a| drv.attrs["UID"] = a }
+  opts.on('-UARG', '--user_agent ARG') { |a| drv.attrs["UserAgent"] = a }
+  opts.on('-vARG', '--version ARG') { |a| drv.attrs["RetsVersion"] = a }
+  opts.on('-h', '--help', 'Show this message') { puts opts ; exit }
+  opts.parse!(ARGV)
+end
+
+if drv.attrs.has_key?('DebugLogFile')
+  drv.attrs['UseDebugLogging'] = "true"
+end
+
+if drv.attrs.has_key?('HttpLogFile')
+  drv.attrs['UseHttpLogging'] = "true"
+end
+
+dbc = ODBC::Database.new
+dbc.drvconnect(drv)
+
+dbc.tables do |stmt|
+  stmt.each_hash do |row|
+    table = row["TABLE_NAME"]
+    puts table
+    
+    dbc.columns(table) do |cstmt|
+      cstmt.each_hash do |crow|
+        puts "\t" + crow["COLUMN_NAME"] + "\t" + crow["TYPE_NAME"]
+      end
+    end
+  end
+end
+
+dbc.disconnect
+
