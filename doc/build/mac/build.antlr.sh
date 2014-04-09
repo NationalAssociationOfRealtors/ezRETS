8e587d19505eaae590d5a1ad1e2f28f4833ea8bf
Keith T. Garner <kgarner@realtors.org>
2014-04-09 17:14:30 -0500

    Should be all the OS X related files

--- doc/build/mac/build.antlr.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-export CXXFLAGS="-mmacosx-version-min=10.4 -isysroot /Developer/SDKs/MacOSX10.4u.sdk -arch i386 -arch ppc -arch x86_64"
-
-export CC=gcc-4.0
-export CXX=g++-4.0
-
-./configure --prefix=/usr/local/encap/antlr-2.7.7 --with-javac=javac
-make
